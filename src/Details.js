f4d4f8703e68998c8565e088b8ad0c0ff3a6f0d8
subhanghani339 <subhanghani339@gmail.com>
2023-08-30 00:55:32 +0500

    Details updated

--- src/Details.js
+++ src/Details.js
@@ -1,28 +1,35 @@
-import { View, Image, ImageBackground } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Image, ImageBackground, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { deviceHeight, deviceWidth } from './Dimensions'
 import Icon from 'react-native-vector-icons/Ionicons';
 import { API_KEY } from './Constants';
 
 const Details = (props) => {
+  const [data, setData] = useState()
   const {name} = props.route.params;
   console.log(props)
 
   useEffect(() => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${API_KEY}`)
     .then(res => res.json())
-    .then(res => console.log(res))
+    .then(res => setData(res))
     .catch(err => console.log(err))
   }, [])
 
   return (
     <View>
-      <ImageBackground source={require("../assests/images/image2.jpg")} style={{height: deviceHeight, width: deviceWidth}} imageStyle={{opacity: 0.6, backgroundColor: '#000'}} />
+      <ImageBackground source={require("../assests/images/image1.jpg")} style={{height: deviceHeight, width: deviceWidth}} imageStyle={{opacity: 0.6, backgroundColor: '#000'}} />
       <View style={{position: 'absolute', paddingVertical: 20, paddingHorizontal: 10}} >
         <View style={{flexDirection: 'row', justifyContent: 'space-between', width: deviceWidth - 20, }}>
             <Icon name="menu" size={46} color="white" />
             <Image source={require('../assests/images/user.jpg')} style={{height: 46, width: 46, borderRadius: 50}} />
         </View>
+      {
+        data ? <View>
+        <Text style={{color: '#fff', fontSize: 40}}>{name}</Text>
+        <Text style={{color: '#fff', fontSize: 22}}>{data[weather][0]['main']}</Text>
+        </View> : null
+      }
       </View>
     </View>
   )
